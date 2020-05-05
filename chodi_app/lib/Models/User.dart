fb8167baac35fec6c18b04c75d4541c9706d62b9
rodgo1314 <36355523+rodgo1314@users.noreply.github.com>
2020-05-04 21:32:27 -0700

    User data class

--- chodi_app/lib/Models/User.dart
+++ chodi_app/lib/Models/User.dart
@@ -4,3 +4,14 @@ class User{
 
   User({this.uid,this.name});
 }
+
+class UserData{
+  final String userId;
+  final String name;
+  final String cityState;
+  final String phoneNumber;
+  final String ageRange;
+  final List<dynamic> userResources;
+  final List<dynamic> userInterest;
+  UserData({this. userId,this.name,this.cityState,this.phoneNumber,this.ageRange,this.userResources,this.userInterest});
+}
