de6d314f0aea3fbf6564633d7c00da57f6a9836d
jrsca <jcallant@uci.edu>
2020-11-16 09:59:01 -0800

    Implemented new interface for EventsTab. Kept in separate v2_ files

--- chodi_app/lib/screens/welcome_wrapper.dart
+++ chodi_app/lib/screens/welcome_wrapper.dart
@@ -2,25 +2,23 @@ import 'package:chodiapp/models/user.dart';
 import 'package:chodiapp/Shared/loading.dart';
 import 'package:chodiapp/screens/Authenticate/finish_sign_up_google_page.dart';
 import 'package:chodiapp/screens/Home/authenticated_home.dart';
+import 'package:chodiapp/screens/Home/v2_authenticated_home.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class WelcomeWrapper extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     UserData userData = Provider.of<UserData>(context);
 
-    if (userData != null){
-      if (userData.name == "New User"){
+    if (userData != null) {
+      if (userData.name == "New User") {
         return FinishSignUpGooglePage();
-      }
-      else{
-        return AuthenticatedHomeScreen();
+      } else {
+        return v2_AuthenticatedHomeScreen();
       }
     }
-    if (userData == null){
+    if (userData == null) {
       return Loading();
     }
   }
