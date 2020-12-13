e1665cc31bb7fa60a2ee92010d7ca55f931883b2
alejandrogznz <alejandrogzn@gmail.com>
2020-12-13 14:40:21 -0800

    Implemented a function that gets the distance from / to the user to an event

--- chodi_app/lib/screens/home/tab_pages/v2_events_tab/v2_events_Explore.dart
+++ chodi_app/lib/screens/home/tab_pages/v2_events_tab/v2_events_Explore.dart
@@ -2,8 +2,11 @@ import 'package:chodiapp/models/events.dart';
 import 'package:chodiapp/screens/home/tab_pages/v2_events_tab/v2_events_card_lg.dart';
 import 'package:flutter/material.dart';
 import 'package:chodiapp/constants/constants.dart';
+import 'package:geocoder/geocoder.dart';
 import 'package:provider/provider.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'dart:developer';
+import 'package:geolocator/geolocator.dart';
 
 class v2_ExplorePage extends StatefulWidget {
   String givenQuery;
@@ -15,10 +18,14 @@ class v2_ExplorePage extends StatefulWidget {
 }
 
 class _v2_ExplorePage extends State<v2_ExplorePage> {
+  double _currentSliderValue = 20;
+  Position _userPosition;
+
   @override
   Widget build(BuildContext context) {
     List<Events> eventsList = Provider.of<List<Events>>(context);
     List<Events> queryList = filterList(eventsList, widget.givenQuery);
+    _determineUserPosition();
 
     if (widget.givenQuery == "")
       return SafeArea(
@@ -26,6 +33,23 @@ class _v2_ExplorePage extends State<v2_ExplorePage> {
           child: ListView(
             physics: BouncingScrollPhysics(),
             children: <Widget>[
+              Slider(
+                value: _currentSliderValue,
+                min: 20,
+                max: 100,
+                divisions: 10,
+                label: _currentSliderValue.round().toString() + " miles",
+                onChanged: (double value) {
+                  setState(() {
+                    _currentSliderValue = value;
+                  });
+                },
+                onChangeEnd: (double value) {
+                  log('data: $_currentSliderValue miles');
+                  log('user position $_userPosition.latitude $_userPosition.longitude');
+                  _showToast(context);
+                }
+              ),
               SizedBox(height: 30),
               if (eventsList.length > 0)
                 (Padding(
@@ -86,19 +110,122 @@ class _v2_ExplorePage extends State<v2_ExplorePage> {
       );
   }
 
-  List<Events> filterList(List<Events> someList, String someQuery) {
+  List<Events> filterList(List<Events> someList, String someQuery){
     return someList.where((someEventInList) {
+      String fullAddress = _buildAddress(someEventInList);
+      bool isEventInRange = false;
       var theQuery = someQuery.toLowerCase();
       var eventTitle = someEventInList.eventName.toLowerCase();
       var eventOrg = someEventInList.orgName.toLowerCase();
       var eventCity = someEventInList.locationProperties.city.toLowerCase();
       var eventState = someEventInList.locationProperties.state.toLowerCase();
       var eventCat = someEventInList.category.toLowerCase();
+
+      // I still haven't figured out the synchronizing, you might get some values too late.
+      // Maybe we can map the distances once to the events at the initial building of the widget
+      _eventIsInRangeOfUser(fullAddress).then((value) => isEventInRange);
+      log('event is in range return $isEventInRange');
+
       return (eventTitle.contains(theQuery) ||
           eventOrg.contains(theQuery) ||
           eventCity.contains(theQuery) ||
           eventState.contains(theQuery) ||
-          eventCat.contains(theQuery));
+          eventCat.contains(theQuery)) ;
     }).toList();
   }
+
+  void _showToast(BuildContext context) {
+    final scaffold = Scaffold.of(context);
+    scaffold.showSnackBar(
+        SnackBar(content: Text(_currentSliderValue.toString() + " miles"))
+    );
+  }
+
+  Future<bool> _eventIsInRangeOfUser(String _eventAddress) async {
+    Coordinates eCoordinates = await _getEventCoordinates(_eventAddress);
+
+    // Assume that this is an online or similar event if the Coordinates are null
+    if (eCoordinates == null) {
+      log('event is in range val is null');
+      return true;
+    }
+    double eLatitude = eCoordinates.latitude;
+    double eLongitude = eCoordinates.longitude;
+    double uLatitude = _userPosition.latitude;
+    double uLongitude = _userPosition.longitude;
+    double distanceInMeters =  Geolocator.distanceBetween(uLatitude, uLongitude, eLatitude, eLongitude);
+    log('distance in meters = $distanceInMeters m.');
+
+    /*
+      Here you would compare the distance in meters to a value that would be set by the user by a Textbox,
+      Slider, or something similar. Return a true if the user is within range
+     */
+    return true;
+  }
+
+  Future<Coordinates> _getEventCoordinates(final String fullAddress) async {
+    List<Address> address;
+    log('getEventCoordinates for $fullAddress');
+
+    // If there is no address, return null coordinates
+    if (fullAddress.isEmpty) {
+      log('was null');
+      return null;
+    }
+
+    address = await Geocoder.local.findAddressesFromQuery(fullAddress);
+    log(address[0].coordinates.toString());
+
+    // If the lookup returns no coordindates, return a null.
+    if (address != null) {
+      log('successfully returned event coordinates');
+      log(address[0].coordinates.toString());
+      return address[0].coordinates;
+    }
+    return null;
+  }
+
+  String _buildAddress(final Events event)
+  {
+    var eAddress = event.locationProperties.address;
+    var eCity = event.locationProperties.city;
+    var eState = event.locationProperties.state;
+    var eCountry = event.locationProperties.country;
+
+    log("$eAddress $eCity $eState $eCountry".trim());
+    return ("$eAddress $eCity $eState $eCountry".trim());
+  }
+
+  Future<Position> _determineUserPosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      log('Location services are not enabled');
+      return Future.error('Location Services are not enabled');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.deniedForever) {
+      log('Location services are permanently denied');
+      return Future.error('Location services are permanently denied, cannot request permission');
+    }
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission != LocationPermission.whileInUse &&
+          permission != LocationPermission.always) {
+        log('Location permissions are denied');
+        return Future.error(
+            'Location permissions are denied (actual value: $permission).');
+
+      }
+    }
+    log('enabled permissions');
+    _userPosition = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    return Future.error("Successfully got position");
+  }
+
 }
+
